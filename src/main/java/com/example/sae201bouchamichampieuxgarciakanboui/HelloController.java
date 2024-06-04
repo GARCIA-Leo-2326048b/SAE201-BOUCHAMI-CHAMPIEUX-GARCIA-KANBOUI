c98ecc4287b383ce9f5784f7ed1517be522ef090
KANBOUI-Jalil-2326062aa <jalil.kanboui@etu.univ-amu.fr>
2024-06-04 16:30:53 +0200

    Organisation fxml avec tout d'organisé mais bouton temps n'affiche rien

--- src/main/java/com/example/sae201bouchamichampieuxgarciakanboui/HelloController.java
+++ src/main/java/com/example/sae201bouchamichampieuxgarciakanboui/HelloController.java
@@ -2,22 +2,25 @@ package com.example.sae201bouchamichampieuxgarciakanboui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
 public class HelloController {
-    @FXML
-    public ImageView blackRook1;
-    @FXML
-    public GridPane chessBoard;
-
-    /*
-   @FXML
-   private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private MenuButton timeMenuButton;
+
+    @FXML
+    public void initialize() {
+        // Set the initial text for the MenuButton
+        timeMenuButton.setText("10 min");
     }
-     */
+
+    @FXML
+    private void handleTimeChange(javafx.event.ActionEvent event) {
+        MenuItem source = (MenuItem) event.getSource();
+        timeMenuButton.setText(source.getText());
+    }
 }
